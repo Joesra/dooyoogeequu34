82f2b76cf0dafe0117dbfa85be776b231d3c8e8c
Youssra Afkir <afkiry1@hva.nl>
2025-10-30 16:28:24 +0100

    connectie met de database om alles door te sturen

--- app/main/routes.py
+++ app/main/routes.py
@@ -25,11 +25,21 @@ def informatie():
 def offerte():
     # Haalt data op van de form in offerte_aanvraag.html
     if request.method == "POST":
-    naam = request.form["naam"]
-    email = request.form["email"]
-    organisatie = request.form["organisatie"]
-    omschrijving = request.form["omschrijving"]
-    )
+        naam = request.form["naam"]
+        email = request.form["email"]
+        organisatie = request.form("organisatie")
+        omschrijving = request.form["omschrijving"]
+
+        conn = get_db_connection() #verbinding maken met de database
+        cursor = conn.cursor() 
+        query = """
+            INSERT INTO offerte_aanvragen (naam, email, organisatie, omschrijving)
+            VALUES (%s, %s, %s, %s)
+        """
+        cursor.execute(query, (naam, email, organisatie, omschrijving))
+        conn.commit() #slaat de ingevulde variable op in de database
+        cursor.close() 
+        conn.close() #sluit de verbinding met de database af
 
     return render_template("offerte_aanvraag.html")
 
