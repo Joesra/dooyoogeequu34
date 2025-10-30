27be37698f99cabc65a8c04992469b1221a764a9
Youssra Afkir <afkiry1@hva.nl>
2025-10-30 15:52:20 +0100

    connectie maken met database

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,7 +1,17 @@
-from flask import render_template
-
+from flask import render_template, request, redirect
 from app.main import bp
+from app.settings import DATABASE
+import mysql.connector
 
+def get_db_connection():
+    """Connectie maken met mysql database"""
+    return mysql.connector.connect(
+        host=DATABASE["HOST"],
+        user=DATABASE["USER"],
+        password=DATABASE["PASSWORD"],
+        database=DATABASE["NAME"],
+        port=DATABASE["PORT"]
+    )
 
 @bp.route("/home")
 def index():
@@ -14,3 +24,4 @@ def informatie():
 @bp.route("/offerte")
 def offerte():
     return render_template("offerte_aanvraag.html")
+
