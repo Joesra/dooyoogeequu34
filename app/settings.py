f801b9d2fcfaad61d7a52f32c0cc112fbbb58270
Youssra Afkir <afkiry1@hva.nl>
2025-12-15 00:35:24 +0100

    in settings.py correcte termen gezet voor mysqlconnector

--- app/settings.py
+++ app/settings.py
@@ -1,14 +1,14 @@
 import os
 from dotenv import load_dotenv
 
-#.env staat in de bovenliggende map van 'app/'
+# .env staat in de bovenliggende map van 'app/'
 dotenv_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env')
 load_dotenv(dotenv_path)
 
 DATABASE = {
-    'NAME': os.getenv("DATABASE_NAME"),
-    'USER': 'pb3bims2526_dooyoogeequu34',
-    'PASSWORD': os.getenv("DB_PASSWORD"),
-    'HOST': 'db.hbo-ict.cloud',
-    'PORT': 3366
+    "host": "db.hbo-ict.cloud",
+    "user": "pb3bims2526_dooyoogeequu34",
+    "password": os.getenv("DB_PASSWORD"),
+    "database": os.getenv("DATABASE_NAME"),
+    "port": int(os.getenv("DB_PORT")) if os.getenv("DB_PORT") else 3366
 }
