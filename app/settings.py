d7bd8b703f3dad95c045a1c83253da244fa44825
Youssra Afkir <afkiry1@hva.nl>
2025-12-15 00:32:19 +0100

    dotenv_path klopte niet, dit kloppend gemaakt

--- app/settings.py
+++ app/settings.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 
 #.env staat in de bovenliggende map van 'app/'
-dotenv_path = os.path.join(os.path.dirname(os.path.dirname(file)), '.env')
+dotenv_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env')
 load_dotenv(dotenv_path)
 
 DATABASE = {
