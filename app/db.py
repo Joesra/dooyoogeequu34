e7d60a71702337bd06a385c2793293f7b57b08f2
Youssra Afkir <afkiry1@hva.nl>
2026-01-03 15:38:19 +0100

    push 4

--- app/db.py
+++ app/db.py
@@ -1,20 +1,25 @@
-"""Database functionalty"""
-
 import requests
 from flask import current_app
 
-
 def execute_query(query, values=None):
     """Calls the HBO-ICT.cloud API to execute a query."""
-    url = current_app.config["API_URL"]
-    url += "/db"
+    url = current_app.config["API_URL"] + "/db"
     api_key = current_app.config["API_KEY"]
     database = current_app.config["DATABASE"]
 
-    x = requests.post(
-        url=url,
-        json={"query": query, "values": values, "database": database},
-        headers={"Authorization": f"Bearer {api_key}"},
-        timeout=10,
-    )
-    return x.json()
+    try:
+        r = requests.post(
+            url=url,
+            json={"query": query, "values": values, "database": database},
+            headers={"Authorization": f"Bearer {api_key}"},
+            timeout=10,
+        )
+        r.raise_for_status()  # HTTP errors gooien exception
+        try:
+            return r.json()
+        except ValueError:
+            print("JSONDecodeError! Response is geen JSON:", r.text)
+            return []
+    except requests.exceptions.RequestException as e:
+        print("Request error:", e)
+        return []   
